7a6abb1c5b153b4f30887e45f44710d39f87dafd
Ba Toi Nguyen <batoi1271.doe@gmail.com>
2022-03-19 18:30:47 +0700

    notification

---
+++ frontend/src/pages/Notification/RoutesNo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import AllNotification from "./page/AllNotification";
+import Mention from "./page/Mention";
+
+const RoutesNo = () => {
+  return (
+    <Switch>
+      <Route path="/notifications" exact component={AllNotification} />
+      <Route path="/notifications/mention" component={Mention} />
+    </Switch>
+  );
+};
+
+export default RoutesNo;
---
+++ frontend/src/pages/Notification/page/Mention.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mention = () => {
+  return (
+    <div>Mention</div>
+  )
+}
+
+export default Mention
