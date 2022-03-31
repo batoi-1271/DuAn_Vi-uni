4a18910cb1d523227af5349311e70c31557ddf71
Ba Toi Nguyen <batoi1271.doe@gmail.com>
2022-03-31 17:21:05 +0700

    icon more

--- frontend/src/components/RightPane/More/More.jsx
+++ frontend/src/components/RightPane/More/More.jsx
@@ -8,9 +8,9 @@ const More = ({ show, close, title, children }) => {
                 <div className="more" onClick={(e) => e.stopPropagation()}>
                     <main className="more_content">
                         <ul>
-                            <li className='items'><i class="fa-solid fa-gear"/>Setting and private</li>
-                            <li className='items'><i class="fa-solid fa-circle-question"/>Help center</li>
-                            <li className='items'><i class="fa-solid fa-fill-drip"/>Display</li>
+                            <li className='items'><i class="far fa-cogs"></i>Setting and private</li>
+                            <li className='items'><i class="far fa-question-circle"></i>Help center</li>
+                            <li className='items'><i class="fas fa-fill-drip"></i>Display</li>
                         </ul>
                     </main>
                 </div>
