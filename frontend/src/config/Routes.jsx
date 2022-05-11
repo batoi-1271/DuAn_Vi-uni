8fea2a53db669fa75da500ff177be7625444e704
haotran21 <tranvanhao10a9@gmail.com>
2022-05-11 20:23:31 +0700

    khong nho

--- frontend/src/config/Routes.jsx
+++ frontend/src/config/Routes.jsx
@@ -1,46 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Switch, Route } from 'react-router-dom';
-import Friends from '../components/MyFriends/Friends';
+import { Switch, Route } from "react-router-dom";
+import Friends from "../components/MyFriends/Friends";
 
-import Home from '../pages/Home/Home';
-import Messages from '../pages/Message/Messages';
-import Notification from '../pages/Notification/Notification';
-import Profile from '../pages/Profile/Profile';
-import Save from '../pages/Saved/Save';
-
+import Home from "../pages/Home/Home";
+import Messages from "../pages/Message/Messages";
+import Notification from "../pages/Notification/Notification";
+import Profile from "../pages/Profile/Profile";
+import Save from "../pages/Saved/Save";
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Route
-                path='/profile'
-                component={Profile}
-            />
-            <Route
-                path='/friends'
-                component={Friends}
-            />
-            <Route
-                path='/saved'
-                component={Save}
-            />
-            <Route
-                path='/message'
-                component={Messages}
-            />
-            <Route
-                path='/notifications'
-                component={Notification}
-            />
-            <Route
-                path='/'
-                exact
-                component={Home}
-            />
-            
-        </Switch>
-    );
-}
+  return (
+    <Switch>
+      <Route path="/profile" component={Profile} />
+      <Route path="/friends" component={Friends} />
+      <Route path="/saved" component={Save} />
+      <Route path="/Message" component={Messages} />
+      <Route path="/notifications" component={Notification} />
+      <Route path="/" exact component={Home} />
+    </Switch>
+  );
+};
 
 export default Routes;
