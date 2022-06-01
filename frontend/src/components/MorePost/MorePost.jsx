bf7f4c93d00ceac582f570b540ee17cf761850ac
Megnut <dntung2409@gmail.com>
2022-06-01 21:57:19 +0700

    modal-img

--- frontend/src/components/MorePost/MorePost.jsx
+++ frontend/src/components/MorePost/MorePost.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import React, { useRef, useState } from 'react';
+
 import { Link } from "react-router-dom";
-import './morepost.scss';
+import Editpost from "../EditPost/Editpost";
+import "./morepost.scss";
 
 const MorePost = (props) => {
-    const fullName = props.dataFromParent;
-    return (
-        <div className="dropContainer">
-            <div className="unfollow dropContainer_content">
-                <i class="fal fa-user-times"></i>
-                <Link to="#">Unfollow <span>@{fullName != null ? fullName : null}</span></Link>
-            </div>
-            <div className="unfollow dropContainer_content">
-                <i class="fal fa-file-edit"></i>
-                <Link to="#">Edit post</Link>
-            </div>
-            <div className="unfollow dropContainer_content">
-                <i class="fal fa-trash"></i>
-                <Link to="#">Delete</Link>
-            </div>
-            <div className="report dropContainer_content">
-                <i class="fal fa-flag"></i>
-                <Link to="#">Report post</Link>
-            </div>
-            <div className="save dropContainer_content">
-                <i class="fal fa-bookmark"></i>
-                <Link to="#">Save post</Link>
-            </div>
-        </div>
-    )
-}
+    const [modal, setModal] = useState(false);
+  const Toggle = () => setModal(!modal);
 
-export default MorePost
+  const fullName = props.dataFromParent;
+  return (
+    <div className="dropContainer">
+      <div className="unfollow dropContainer_content">
+        <i class="fal fa-user-times"></i>
+        <Link to="#">
+          Unfollow <span>@{fullName != null ? fullName : null}</span>
+        </Link>
+      </div>
+      <div className="unfollow dropContainer_content">
+        <i class="fal fa-file-edit"></i>
+        {/* <Link to="#">Edit post</Link> */}
+        <button variant="contained" onClick={() => Toggle()}>
+            Edit post
+        </button>
+        <Editpost show={modal} close={Toggle} title="">
+          <h3>Hello</h3>
+        </Editpost>
+      </div>
+      <div className="unfollow dropContainer_content">
+        <i class="fal fa-trash"></i>
+        <Link to="#">Delete</Link>
+      </div>
+      <div className="report dropContainer_content">
+        <i class="fal fa-flag"></i>
+        <Link to="#">Report post</Link>
+      </div>
+      <div className="save dropContainer_content">
+        <i class="fal fa-bookmark"></i>
+        <Link to="#">Save post</Link>
+      </div>
+    </div>
+  );
+};
+
+export default MorePost;
